0ee417f7f7030cea33c8b8ea3ccd3055dddeb0cc
joseph <josephjunejoe@yahoo.com>
2021-10-12 01:38:37 +0800

    cors thing

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 //   res.set('Access-Control-Allow-Origin', '*');
 //   next();
 // });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('welcome to the recas-api endpoint.');
