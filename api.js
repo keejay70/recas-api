4be23bb1ecc480c44c68e704f06eee809c8c755b
joseph <josephjunejoe@yahoo.com>
2021-10-13 02:33:16 +0800

    sdadad

--- api.js
+++ api.js
@@ -252,7 +252,7 @@ const getOneCrime = async (request, response) => {
     var id = request.body.id;
     console.log(id)
 
-    var sql = "SELECT * from crimes JOIN crimetype ON crimes.crimeType_id = crimetype.id WHERE crimes.id="+integer(id);
+    var sql = "SELECT * from crimes JOIN crimetype ON crimes.crimeType_id = crimetype.id WHERE crimes.id="+parseInt(id);
 
     var returnObj = {
         status:1,
